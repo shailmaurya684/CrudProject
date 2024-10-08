31fda584312a06d551d592f0857ef76221cba6cf
shailmaurya684 <shailmaurya684@gmail.com>
2024-10-08 20:17:01 +0530

    adding source code

---
+++ fullstack-backend/src/main/java/com/crudoperation/fullstack_backend/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.crudoperation.fullstack_backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crudoperation.fullstack_backend.model.User;
+import com.crudoperation.fullstack_backend.repository.UserRepository;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping("/user")
+	User newUser(@RequestBody User newUser) {
+		
+		return userRepository.save(newUser);
+	}
+	
+	@GetMapping("/users")
+	List<User> getAllUsers(){
+		return userRepository.findAll();
+	}
+	
+	
+}
